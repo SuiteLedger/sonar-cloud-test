99405ad52f6ec112df06c9e7836f6eead040e4cc
= <nniroshan@palo-it.com>
2024-03-10 08:59:27 +0800

    add code vulnerable to sql injection to test sonar cloud

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
 echo "Hello World!";
+
+$host = 'localhost';
+$username = 'your_database_username';
+$password = 'your_database_password';
+$database = 'your_database_name';
+
+// Create connection
+$conn = new mysqli($host, $username, $password, $database);
+
+$user_id = $_GET['id'];
+// Unsafe SQL query without using prepared statements or parameterized queries
+$sql = "SELECT * FROM users WHERE id = $user_id";
+$result = mysqli_query($conn, $sql);
